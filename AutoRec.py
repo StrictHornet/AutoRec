52eefd6e18f6f736c014ea1f1462680554795d4a
StrictHornet <42406510+StrictHornet@users.noreply.github.com>
2020-10-22 21:39:00 +0100

    Build update.

--- AutoRec.py
+++ AutoRec.py
@@ -1,5 +1,65 @@
 import tkinter as tk
 
+#
+# from openpyxl import Workbook
+# from openpyxl import load_workbook
+
+# workbook = load_workbook(
+#     filename="C:/Users/okosu/Google Drive/Work/VODACOM MAIN COPY OF SURVEY SPREADSHEET.xlsx")
+
+# ############# TASKS ###################
+# # PUTTING EVERYTHING IN ONE CONSTANT FILE
+# # CREATE NEW FILE, COPY REC FILE, REC
+# # GUI
+
+# # For loop for search and cell assignment
+# for ir in workbook["RecNew"].iter_rows(min_row=2, min_col=3):
+#     # print(ir) # For debugging
+#     for sheet in workbook:
+#         for row in sheet.iter_rows(min_row=2, min_col=8):
+#             # print(row) # For debugging
+#             try:
+#                 if ir[0].value == row[0].value:
+#                     if row[6].value != "NIL":
+#                         # print("Present") # For debugging
+#                         # print("VBN is {} and Recon is {}".format(
+#                         #     row[0].value, ir[0].value))
+#                         ir[21].value = "Failed"  # 19
+#                         ir[22].value = row[6].value  # 20
+#                         break  # Since IR has been found loop should break to next IR
+#                     else:
+#                         ir[21].value = "OK"
+#             except:
+#                 print("Didn't work!")
+#         else:
+#             continue
+#         break  # Break the outer loop
+
+# # For loop for duplicate search
+# for ir in workbook["RecNew"].iter_rows(min_row=2, min_col=3):
+#     for sheet in workbook:
+#         for row in sheet.iter_rows(min_row=2, min_col=3):
+#             try:
+#                 if ir[0].value == row[0].value:
+#                     if row[21].value == "Approved":
+#                         # print("Duplicate") # For debugging
+#                         # print("VBN is {} and Recon is {}".format(
+#                         #     row[0].value, ir[0].value))
+#                         ir[21].value = "DUPLICATE"  # 19
+#                         ir[22].value = row[22].value  # 20
+#                         break  # Since IR has been found loop should break to next IR
+#             except:
+#                 print("Didn't work!")
+#         else:
+#             continue
+#         break  # Break the outer loop
+
+# workbook.save(
+#     filename="C:/Users/okosu/Google Drive/Work/VODACOM MAIN COPY OF SURVEY SPREADSHEET.xlsx")
+
+
+#
+
 win = tk.Tk()
 win.title("AutoRec")
 
@@ -13,10 +73,10 @@ lbl_greeting.pack(fill=tk.X)
 frm_body = tk.Frame()
 frm_body.pack(padx=15, pady=15)
 
-lbl_val1 = tk.Label(text = "Value 1", master = frm_body)
+lbl_val1 = tk.Label(text = "inq. Approval Column Aplhabet Identifier", master = frm_body)
 lbl_val1.grid(row=0, column=0)
 
-lbl_val2 = tk.Label(text = "Value 2", master = frm_body)
+lbl_val2 = tk.Label(text = "inq. Comment Column Aplhabet Identifier", master = frm_body)
 lbl_val2.grid(row=1, column=0)
 
 ent1 = tk.Entry(master = frm_body)

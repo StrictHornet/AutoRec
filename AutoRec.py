45ace6805149d266449f9d46646ccbbd701ab737
StrictHornet <42406510+StrictHornet@users.noreply.github.com>
2020-10-24 11:16:15 +0100

    Frame Design 1.0

--- AutoRec.py
+++ AutoRec.py
@@ -66,7 +66,9 @@ win.title("AutoRec")
 frm_head = tk.Frame(bg="yellow")
 frm_head.pack(fill=tk.X)
 
-lbl_greeting = tk.Label(relief=tk.RIDGE, height=2, bg="red",text="Inq SLA Monthly Reconciler",
+frm_directory = tk.Frame(padx = 10,pady = 10)
+
+lbl_greeting = tk.Label(relief=tk.RIDGE, height=2, bg="#641822",text="Inq SLA Monthly Reconciler",
                             master=frm_head)
 lbl_greeting.pack(fill=tk.X)
 
@@ -79,19 +81,25 @@ lbl_val1.grid(row=0, column=0)
 lbl_val2 = tk.Label(text = "inq. Comment Column Aplhabet Identifier", master = frm_body)
 lbl_val2.grid(row=1, column=0)
 
-ent1 = tk.Entry(master = frm_body)
+ent1 = tk.Entry(master = frm_body, width = 5)
 ir21 = ent1.get()
 
-ent2 = tk.Entry(master = frm_body)
+ent2 = tk.Entry(master = frm_body, width = 5)
 ir22 = ent2.get()
 
 ent1.grid(row=0, column=1)
 ent2.grid(row=1, column=1)
 
-lbl_directory = tk.Label(width=35, bg="gray", padx=10, text="DIRECTORY PATH")
+lbl_directory = tk.Label(master = frm_directory, width=35, bg="gray", padx=10, text="DIRECTORY PATH")
 lbl_directory.pack()
 
-rec = tk.Button(text="Reconcile", bg="red", width=15, height=3)
-rec.pack()
+directory_ent = tk.Entry(master=frm_directory, width=55)
+AR_directory = directory_ent.get()
+directory_ent.pack()
+directory_ent.insert(0, "---------------COPY AND PASTE DIRECTORY HERE------------------")
+frm_directory.pack()
+
+rec = tk.Button(text="Reconcile", bg="#a00008", width=15, height=3)
+rec.pack(pady=5)
 
 win.mainloop()

f452c09cc5d9cf68a17f032ffb8d31eca0e81116
StrictHornet <42406510+StrictHornet@users.noreply.github.com>
2020-11-25 19:59:51 +0100

    Feature Updates

--- AutoRec.py
+++ AutoRec.py
@@ -5,66 +5,54 @@ from openpyxl import load_workbook
 ############# TASKS ###################
 # PUTTING EVERYTHING IN ONE CONSTANT FILE
 # CREATE NEW FILE, COPY REC FILE, REC
-# GUI
+## GUI
 # DUPLICATE RESULT IR
-# CONVERT ALPHABET TO NUMBER
+## CONVERT ALPHABET TO NUMBER
 # DESIGN ERROR TEXT FIELD
 # PRINT ERRORS TO TEXT FIELD
+# "COMPLETED RECONCILIATION" TEXTFIELD
 
-#
-
-# #FUNCTION THAT RECONCILES SURVEY DOCUMENT#
+#FUNCTION THAT RECONCILES SURVEY DOCUMENT#
 def reconcile():
-    print("clicked")
-#     workbook = load_workbook(
-#     filename="C:/Users/okosu/Google Drive/Work/VODACOM MAIN COPY OF SURVEY SPREADSHEET.xlsx")
+    app = (ord(approvalColumn.lower()) - 96) - 3 #3 is the distance between the beginning of the ir that starts from the third column plus the one unit diffrence in the ir[] array
+    com = (ord(commentColumn.lower()) - 96) - 3
+    workbook = load_workbook(
+    filename="C:/Users/okosu/Google Drive/Work/VODACOM MAIN COPY OF SURVEY SPREADSHEET.xlsx")
 
-# # For loop for search and cell assignment
-#     for ir in workbook["RecNew"].iter_rows(min_row=2, min_col=3):
-#         # print(ir) # For debugging
-#         for sheet in workbook:
-#             for row in sheet.iter_rows(min_row=2, min_col=8):
-#                 # print(row) # For debugging
-#                 try:
-#                     if ir[0].value == row[0].value:
-#                         if row[6].value != "NIL":
-#                             # print("Present") # For debugging
-#                             # print("VBN is {} and Recon is {}".format(
-#                             #     row[0].value, ir[0].value))
-#                             ir[21].value = "Failed"  # 19
-#                             ir[22].value = row[6].value  # 20
-#                             break  # Since IR has been found loop should break to next IR
-#                         else:
-#                             ir[21].value = "OK"
-#                 except:
-#                     print("Didn't work!")
-#             else:
-#                 continue
-#             break  # Break the outer loop
+# For loop for search and cell assignment
+    for ir in workbook["RecNew"].iter_rows(min_row=2, min_col=3):
+        for sheet in workbook:
+            for row in sheet.iter_rows(min_row=2, min_col=8):
+                try:
+                    if ir[0].value == row[0].value:
+                        if row[6].value != "NIL":
+                            ir[app].value = "Failed" 
+                            ir[com].value = row[6].value 
+                            break  # Since IR has been found loop should break to next IR
+                        else:
+                            ir[app].value = "OK"
+                except:
+                    print("Didn't work!")
+            else:
+                continue
+            break  # Break the outer loop
 
-# # For loop for duplicate search
-#     for ir in workbook["RecNew"].iter_rows(min_row=2, min_col=3):
-#         for sheet in workbook:
-#             for row in sheet.iter_rows(min_row=2, min_col=3):
-#                 try:
-#                     if ir[0].value == row[0].value:
-#                         if row[21].value == "Approved":
-#                             # print("Duplicate") # For debugging
-#                             # print("VBN is {} and Recon is {}".format(
-#                             #     row[0].value, ir[0].value))
-#                             ir[21].value = "DUPLICATE"  # 19
-#                             ir[22].value = row[22].value  # 20
-#                             break  # Since IR has been found loop should break to next IR
-#                 except:
-#                     print("Didn't work!")
-#             else:
-#                 continue
-#             break  # Break the outer loop
-
-# workbook.save(
-#     filename="C:/Users/okosu/Google Drive/Work/VODACOM MAIN COPY OF SURVEY SPREADSHEET.xlsx")
-
-#
+# For loop for duplicate search
+    for ir in workbook["RecNew"].iter_rows(min_row=2, min_col=3):
+        for sheet in workbook:
+            for row in sheet.iter_rows(min_row=2, min_col=3):
+                try:
+                    if ir[0].value == row[0].value:
+                        if row[app].value != "":
+                            ir[app].value = "DUPLICATE" 
+                            break  # Since IR has been found loop should break to next IR
+                except:
+                    print("Didn't work!")
+            else:
+                continue
+            break  # Break the outer loop
+    workbook.save(
+        filename="C:/Users/okosu/Google Drive/Work/VODACOM MAIN COPY OF SURVEY SPREADSHEET.xlsx")
 
 win = tk.Tk()
 win.title("AutoRec")
@@ -79,15 +67,19 @@ lbl_greeting.pack(fill=tk.X)
 frm_body = tk.Frame()
 frm_body.pack(padx=15, pady=15)
 
+approvalColumn = "X"
 lbl_approvalColumn = tk.Label(text = "inq. Approval Column Alphabet Identifier", master = frm_body)
 lbl_approvalColumn.grid(row=0, column=0)
 ent_approvalColumn = tk.Entry(master = frm_body, width = 5)
+ent_approvalColumn.insert(0, approvalColumn)
 approvalColumn = ent_approvalColumn.get()
 ent_approvalColumn.grid(row=0, column=1)
 
+commentColumn = "Y"
 lbl_commentColumn = tk.Label(text = "inq. Comment Column Alphabet Identifier", master = frm_body)
 lbl_commentColumn.grid(row=1, column=0)
 ent_commentColumn = tk.Entry(master = frm_body, width = 5)
+ent_commentColumn.insert(0, commentColumn)
 commentColumn = ent_commentColumn.get()
 ent_commentColumn.grid(row=1, column=1)
 
@@ -97,10 +89,10 @@ lbl_directory = tk.Label(master = frm_directory, width=35, bg="gray", padx=10, t
 lbl_directory.pack()
 
 file_directory = "C:/Users/okosu/Google Drive/Work/VODACOM MAIN COPY OF SURVEY SPREADSHEET.xlsx"
-directory_ent = tk.Entry(master=frm_directory, width=55)
-directory_ent.insert(0, file_directory)
-file_directory = directory_ent.get()
-directory_ent.pack()
+ent_directory = tk.Entry(master=frm_directory, width=55)
+ent_directory.insert(0, file_directory)
+file_directory = ent_directory.get()
+ent_directory.pack()
 
 frm_directory.pack()
 
